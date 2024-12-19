b5e7d7ba9694dbe78c2ffe7f99adb38d047be71a
Eliyamaulidda <112456281+Eliyamaulidda@users.noreply.github.com>
2024-12-19 19:03:02 +0700

    update js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,46 @@
 // Ini Javascript
 console.log('Javascript berhasil tersambung');
 
-console.log('Javascript berhasil tersambung');
-
 let indexSlide = 0;
 
-nextSlide(); 
+nextSlide();
 
-// Fungsi untuk melanjutkan slide ke berikutnya
-function nextSlide() {
-    showBanner(indexSlide += 1); 
+// Fungsi untuk memvalidasi
+function validateForm() {
+    const usernameInput = document.getElementById('username-input').value;
+    console.log(usernameInput);
+
+    // Validasi jika nama user tidak kosong
+    if (usernameInput == '') {
+        alert('Inputan tidak boleh kosong');
+    } else {
+        document.getElementById('username-result').innerHTML = usernameInput;
+    }
+
+    console.log('validateForm executed');
 }
 
-// Fungsi untuk menampilkan slide
+function nextSlide() {
+    showBanner(indexSlide += 1);
+}
+
 function showBanner(n) {
-    const imageList = document.getElementsByClassName('banner-slide'); 
+    const imageList = document.getElementsByClassName('banner-img');
 
-    if (n > imageList.length - 1) indexSlide = 0; 
+    console.log(imageList);
+    console.log(imageList.length);
+    console.log(n);
+    if (n > imageList.length - 1) indexSlide = 0;
 
+
+    // Hide semua banner
     for (let i = 0; i < imageList.length; i++) {
         imageList[i].style.display = "none";
     }
 
+    // Show 1 Banner sesuai Posisi yang diinginkan
     imageList[indexSlide].style.display = "block";
 }
 
-// Automatisasi perubahan slide setiap 3 detik
+// Automate banned slide
 setInterval(nextSlide, 3000);
